b9b6bb6d584fd872dbf44a7fb51ab5f80d8c5f83
Aaron de Boer <aarondeboerwk@gmail.com>
2022-11-15 18:59:09 +0100

    added drawing functionality

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ let currentSize = 10;
 let bucketColor = '#FFFFFF';
 let currentColor = '#A51DAB';
 let isEraser = false;
-// let isMouseDown = false;
-// let drawnArray = [];
+let isMouseDown = false;
+let drawnArray = [];
 
 // Formatting Brush Size
 function displayBrushSize() {
@@ -66,7 +66,9 @@ function switchToBrush() {
   activeToolEl.textContent = 'Brush';
   brushIcon.style.color = 'black';
   eraser.style.color = 'white';
+  console.log(currentColor);
   currentColor = `#${brushColorBtn.value}`;
+  console.log(currentColor);
   currentSize = 10;
   brushSlider.value = 10;
   displayBrushSize();
@@ -134,21 +136,19 @@ function getMousePosition(event) {
 canvas.addEventListener('mousedown', (event) => {
   isMouseDown = true;
   const currentPosition = getMousePosition(event);
-  console.log('mouse is clicked', currentPosition);
-//   context.moveTo(currentPosition.x, currentPosition.y);
-//   context.beginPath();
-//   context.lineWidth = currentSize;
-//   context.lineCap = 'round';
-//   context.strokeStyle = currentColor;
+  context.moveTo(currentPosition.x, currentPosition.y);
+  context.beginPath();
+  context.lineWidth = currentSize;
+  context.lineCap = 'round';
+  context.strokeStyle = currentColor;
 });
 
 // Mouse Move
 canvas.addEventListener('mousemove', (event) => {
   if (isMouseDown) {
     const currentPosition = getMousePosition(event);
-    console.log('mouse is moving', currentPosition);
-  //   context.lineTo(currentPosition.x, currentPosition.y);
-  //   context.stroke();
+    context.lineTo(currentPosition.x, currentPosition.y);
+    context.stroke();
   //   storeDrawn(
   //     currentPosition.x,
   //     currentPosition.y,
@@ -164,7 +164,6 @@ canvas.addEventListener('mousemove', (event) => {
 // Mouse Up
 canvas.addEventListener('mouseup', () => {
   isMouseDown = false;
-  console.log('mouse is unclicked');
 });
 
 // // Save to Local Storage
